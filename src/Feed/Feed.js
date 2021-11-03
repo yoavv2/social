c6310a2ed5d77617da20ec0b1ddfdf5c7bc828fb
yoav <23hutc8@gmail.com>
2021-11-03 18:39:06 +0200

    first commit

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Grid } from "semantic-ui-react";
 function Feed() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Grid columns={3}>
+      <Grid.Row>
+        <Grid.Column>
+          <h1>Recent Posts</h1>
+        </Grid.Column>
+        <Grid.Column></Grid.Column>
+      </Grid.Row>
+
+      <Grid.Row>
+        <Grid.Column></Grid.Column>
+        <Grid.Column></Grid.Column>
+        <Grid.Column></Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 }
 
-export default Feed
+export default Feed;
